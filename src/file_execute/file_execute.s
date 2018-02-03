4204e7e446975fbcbf289dda872c1f6b93bc4938
The6P4C <watsonjcampbell@gmail.com>
2018-02-03 21:43:49 +1000

    Improve file_execute with a macro

--- src/file_execute/file_execute.s
+++ src/file_execute/file_execute.s
@@ -1,5 +1,14 @@
-! r9 - function call
-! r12 - file descriptor
+.macro print_error error_string
+	mov r0, r4
+	mova \error_string, r0
+	mov r0, r5
+
+	mov.l PrintStandardErrorString, r0
+	jsr @r0
+	nop
+.endm
+
+! r8 - file descriptor
 ! all delay slots are nops cause i can't be bothered
 
 ! Open the file and get a file descriptor
@@ -7,76 +16,48 @@ mova file_path, r0
 mov r0, r4
 mov #5, r5
 
-mov.l open, r9
-jsr @r9
+mov.l open, r0
+jsr @r0
 nop
 
 ! Save our file descriptor so it's not overwritten
-mov r0, r12
+mov r0, r8
 
-! Print the error if there was one
-mov r0, r4
-mova open_str, r0
-mov r0, r5
-
-mov.l PrintStandardErrorString, r9
-jsr @r9
-nop
+print_error open_str
 
 ! Run fstat to get the file size
-mov r12, r4
+mov r8, r4
 mov.l buf, r5
 
-mov.l fstat, r9
-jsr @r9
+mov.l fstat, r0
+jsr @r0
 nop
 
-! Print the error if there was one
-mov r0, r4
-mova fstat_str, r0
-mov r0, r5
-
-mov.l PrintStandardErrorString, r9
-jsr @r9
-nop
+print_error fstat_str
 
 ! Read the file into our buffer
-mov r12, r4
+mov r8, r4
 mov.l buf, r5
 mov.l @(4,r5), r6 ! 4 bytes into the fstat struct is the file size in bytes
 
-mov.l read, r9
-jsr @r9
+mov.l read, r0
+jsr @r0
 nop
 
-! Print the error if there was one
-mov r0, r4
-mova read_str, r0
-mov r0, r5
-
-mov.l PrintStandardErrorString, r9
-jsr @r9
-nop
+print_error read_str
 
 ! Close the file
-mov r12, r4
+mov r8, r4
 
-mov.l close, r9
-jsr @r9
+mov.l close, r0
+jsr @r0
 nop
 
-! Print the error if there was one
-mov r0, r4
-mova close_str, r0
-mov r0, r5
-
-mov.l PrintStandardErrorString, r9
-jsr @r9
-nop
+print_error close_str
 
 ! Jump to the code we just loaded, as a subroutine/function
-mov.l buf, r9
-jsr @r9
+mov.l buf, r0
+jsr @r0
 nop
 
 ! Hang, because returning to the function is too hard
