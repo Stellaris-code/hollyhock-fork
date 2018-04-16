d1c89ab367c511578582bf15d6ee662f4448c773
The6P4C <watsonjcampbell@gmail.com>
2018-04-16 22:03:38 +1000

    Add Debug_GetKey function

--- sdk/include/sdk/sys/debug.h
+++ sdk/include/sdk/sys/debug.h
@@ -4,6 +4,7 @@
 
 void (*const Debug_DrawScreen)() = (void *) 0x8003733E;
 int (*const Debug_GetCursorPosition)(int *x, int *y) = (void *) 0x8002E448;
+int (*const Debug_GetKey)() = (void *) 0x80094380;
 void (*const Debug_PrintStandardErrorString)(int retVal, char *functionName) = (void *) 0x80065998;
 bool (*const Debug_PrintString)(char *string, bool invert) = (void *) 0x8002DA0C;
 int (*const Debug_SetCursorPosition)(int x, int y) = (void *) 0x8002E430;
