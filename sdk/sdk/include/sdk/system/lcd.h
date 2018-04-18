57abd8a6fd42bb73c05db17cf5f6e80c75ecc832
The6P4C <watsonjcampbell@gmail.com>
2018-04-18 16:44:04 +1000

    Add LCD function to set color from palette

--- sdk/sdk/include/sdk/system/lcd.h
+++ sdk/sdk/include/sdk/system/lcd.h
@@ -3,6 +3,39 @@
 #include <stdint.h>
 
 /**
+ * Palette color passed to @ref LCD_SetPixelFromPalette.
+ */
+#define PALETTE_BLACK 0
+/**
+ * Palette color passed to @ref LCD_SetPixelFromPalette.
+ */
+#define PALETTE_BLUE 1
+/**
+ * Palette color passed to @ref LCD_SetPixelFromPalette.
+ */
+#define PALETTE_GREEN 2
+/**
+ * Palette color passed to @ref LCD_SetPixelFromPalette.
+ */
+#define PALETTE_CYAN 3
+/**
+ * Palette color passed to @ref LCD_SetPixelFromPalette.
+ */
+#define PALETTE_RED 4
+/**
+ * Palette color passed to @ref LCD_SetPixelFromPalette.
+ */
+#define PALETTE_MAGENTA 5
+/**
+ * Palette color passed to @ref LCD_SetPixelFromPalette.
+ */
+#define PALETTE_YELLOW 6
+/**
+ * Palette color passed to @ref LCD_SetPixelFromPalette.
+ */
+#define PALETTE_WHITE 7
+
+/**
  * Converts three RGB values into one RGB565 value.
  *
  * @param r The red component, between 0 and 31 (0x1F) inclusive.
@@ -82,5 +115,16 @@ void LCD_Refresh();
  * @param color The color to set the pixel, in RGB565 format.
  */
 void LCD_SetPixel(int x, int y, uint16_t color);
+
+/**
+ * Sets the color of a pixel, from a pre-defined palette. Result is not visible
+ * until @ref LCD_Refresh is called. Index is from macros beginning with
+ * @c PALETTE_.
+ *
+ * @param x The X coordinate of the pixel.
+ * @param y The Y coordinate of the pixel.
+ * @param index The index of the color in the palette to use.
+ */
+void LCD_SetPixelFromPalette(int x, int y, uint8_t index);
 
 #endif
