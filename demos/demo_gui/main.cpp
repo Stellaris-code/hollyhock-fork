146bbd078c71f2d547a6a8314d690c5d968b8190
The6P4C <watsonjcampbell@gmail.com>
2018-06-29 15:44:40 +1000

    Move to C++ GUIs!!!

--- demos/demo_gui/main.cpp
+++ demos/demo_gui/main.cpp
@@ -1,84 +1,51 @@
-#include <sdk/os/debug.hpp>
-#include <sdk/os/file.hpp>
 #include <sdk/os/gui.hpp>
-#include <sdk/os/lcd.hpp>
 
 void main() {
-	// Put your code here.
-	struct GUI_Dialog *dialog = GUI_CreateDialog(
-		0,
-		DIALOG_HEIGHT_25, DIALOG_ALIGN_CENTER,
+	GUIDialog dialog(
+		GUIDialog::Height25, GUIDialog::AlignTop,
 		"Dialog title",
-		0, 0,
-		DIALOG_KBD_NONE
+		GUIDialog::KeyboardStateABC
 	);
 
-	// Add some labels
-	uint16_t r = 0b1111100000000000;
-	uint16_t g = 0b0000011111100000;
-	uint16_t b = 0b0000000000011111;
-
-	void *label0 = GUI_CreateLabel(
-		0,
-		dialog->leftX + 10,
-		dialog->topY + 10,
-		"Label 0",
-		0,
-		LABEL_BACKGROUND | LABEL_SELECTABLE,
-		0, 
-		&r, &g, true, b,
-		0
+	GUILabel label0(
+		dialog.GetLeftX() + 10,
+		dialog.GetTopY() + 10,
+		"Label 0"
 	);
-	dialog->functions->AddElement(dialog, label0, 0);
+	dialog.AddElement(label0);
 
-	void *label1 = GUI_CreateLabel(
-		0,
-		dialog->leftX + 10,
-		dialog->topY + 50,
-		"Label 1",
-		0,
-		LABEL_BACKGROUND | LABEL_SELECTABLE,
-		0, 
-		&r, &g, true, b,
-		0
+	GUILabel label1(
+		dialog.GetLeftX() + 10,
+		dialog.GetTopY() + 50,
+		"Label 1"
 	);
-	dialog->functions->AddElement(dialog, label1, 0);
+	dialog.AddElement(label1);
 
-	// Add a textbox
-	void *textBox = GUI_CreateTextBox(
-		0,
-		dialog->leftX + 150,
-		dialog->topY + 10,
+	GUITextBox textBox(
+		dialog.GetLeftX() + 150,
+		dialog.GetTopY() + 10,
 		100,
 		"Textbox!",
-		0,
-		TEXTBOX_EDITABLE | TEXTBOX_DRAW_BOX,
+		GUITextBox::FlagEditable | GUITextBox::FlagDrawBox,
 		100000, false
 	);
-	dialog->functions->AddElement(dialog, textBox, 0);
+	dialog.AddElement(textBox);
 
-	// Add some radio buttons
-	void *radioButton0 = GUI_CreateRadioButton(
-		0,
-		dialog->leftX + 150,
-		dialog->topY + 40,
+	GUIRadioButton radioButton0(
+		dialog.GetLeftX() + 150,
+		dialog.GetTopY() + 40,
 		"Radio button 0",
-		0,
-		RADIOBUTTON_ENABLED | RADIOBUTTON_SELECTED,
-		0, 0
+		GUIRadioButton::FlagEnabled
 	);
-	dialog->functions->AddElement(dialog, radioButton0, 0);
+	dialog.AddElement(radioButton0);
 
-	void *radioButton1 = GUI_CreateRadioButton(
-		0,
-		dialog->leftX + 150,
-		dialog->topY + 60,
+	GUIRadioButton radioButton1(
+		dialog.GetLeftX() + 150,
+		dialog.GetTopY() + 60,
 		"Radio button 1",
-		0,
-		RADIOBUTTON_ENABLED,
-		0, 0
+		GUIRadioButton::FlagEnabled | GUIRadioButton::FlagSelected
 	);
-	dialog->functions->AddElement(dialog, radioButton1, 0);
+	dialog.AddElement(radioButton1);
 
-	dialog->functions->ShowDialog(dialog);
+	dialog.ShowDialog();
 }
--- sdk/include/sdk/os/gui.hpp
+++ sdk/include/sdk/os/gui.hpp
@@ -1,26 +1,23 @@
 /**
  * @file
- * Functions for creating GUI elements, such as dialogs and message boxes.
+ * Functions and classes used to create/display GUI elements.
  *
  * Example: display a simple dialog with a label
  * @code{cpp}
- * struct GUI_Dialog *dialog = GUI_CreateDialog(
- *     0,
- *     DIALOG_HEIGHT_55, DIALOG_ALIGN_CENTER,
- *     "Dialog title",
- *     0, 0,
- *     DIALOG_KBD_NONE
+ * GUIDialog dialog(
+ *     GUIDialog::Height25, GUIDialog::AlignTop,
+ *     "Dialog Title",
+ *     GUIDialog::KeyboardStateABC
  * );
- *
- * void *label = GUI_CreateLabel(
- *     0,
- *     dialog->leftX + 5, dialog->topY + 5,
- *     "Label Text",
- *     0, 0, 0, 0, 0, false, 0, 0
+ * 
+ * GUILabel label(
+ *     dialog.GetLeftX() + 10,
+ *     dialog.GetTopY() + 10,
+ *     "Label Text"
  * );
- * dialog->functions->AddElement(dialog, label, 0);
- *
- * dialog->functions->ShowDialog(dialog);
+ * dialog.AddElement(label);
+ * 
+ * dialog.ShowDialog();
  * @endcode
  */
 
@@ -34,114 +31,6 @@ const int BUTTON_ABORT = 1 << 8;
 const int BUTTON_RETRY = 1 << 9;
 const int BUTTON_CANCEL = 1 << 10;
 
-const int DIALOG_HEIGHT_25 = 0;
-const int DIALOG_HEIGHT_55 = 1;
-const int DIALOG_HEIGHT_75 = 2;
-const int DIALOG_HEIGHT_95 = 3;
-const int DIALOG_HEIGHT_35 = 4;
-const int DIALOG_HEIGHT_60 = 5;
-
-const int DIALOG_ALIGN_TOP = 0;
-const int DIALOG_ALIGN_CENTER = 1;
-const int DIALOG_ALIGN_BOTTOM = 2;
-
-const int DIALOG_KBD_NONE = 0; // 2 gives same effect
-const int DIALOG_KBD_MATH1 = 1; // 3 gives same effect
-const int DIALOG_KBD_MATH2 = 4;
-const int DIALOG_KBD_MATH3 = 5;
-const int DIALOG_KBD_TRIG = 6;
-const int DIALOG_KBD_VAR = 7;
-const int DIALOG_KBD_ABC = 8;
-const int DIALOG_KBD_CATALOG = 9;
-const int DIALOG_KBD_ADVANCE = 10;
-const int DIALOG_KBD_NUMBER = 11;
-
-const int LABEL_BACKGROUND = 1 << 0;
-const int LABEL_SELECTABLE = 1 << 15;
-
-const int RADIOBUTTON_SELECTED = 1 << 2;
-const int RADIOBUTTON_ENABLED = 1 << 15;
-
-const int TEXTBOX_DRAW_BOX = 1 << 3;
-const int TEXTBOX_EDITABLE = 1 << 8;
-
-// Forward declaration - the dialog struct is passed as an argument to some
-// functions in the function table.
-struct GUI_Dialog;
-
-struct GUI_Dialog_FunctionTable {
-	uint8_t unknown0[0x38];
-
-	/**
-	 * Adds a GUI element to the dialog.
-	 *
-	 * @param dialog A pointer to the dialog to add the GUI element to.
-	 * @param element The GUI element to add.
-	 * @param unknown0 Unknown parameter. Value 0 should be passed.
-	 */
-	void (*const AddElement)(struct GUI_Dialog *dialog, void *element, int unknown0);
-
-	uint8_t unknown2[0x158];
-
-	/**
-	 * Presents the dialog to the user.
-	 *
-	 * @param dialog A pointer to the dialog to display.
-	 */
-	void (*const ShowDialog)(struct GUI_Dialog *dialog);
-};
-
-struct GUI_Dialog {
-	uint8_t unknown0[0x10];
-
-	/**
-	 * The X position of the left edge of the dialog body, in pixels.
-	 */
-	uint16_t leftX;
-
-	/**
-	 * The Y position of the top edge of the dialog body, in pixels.
-	 */
-	uint16_t topY;
-
-	/**
-	 * The X position of the right edge of the dialog body, in pixels.
-	 */
-	uint16_t rightX;
-
-	/**
-	 * The Y position of the bottom edge of the dialog body, in pixels.
-	 */
-	uint16_t bottomY;
-
-	uint8_t unknown1[0x34];
-
-	/**
-	 * Table of functions performing a wide range of operations on the dialog.
-	 * For information about each function, see the documentation for
-	 * @c struct @ref GUI_Dialog_FunctionTable.
-	 */
-	struct GUI_Dialog_FunctionTable *functions;
-};
-
-/**
- * Creates a dialog.
- *
- * If 0 is passed for the argument @p dialog, a dialog struct will be allocated
- * inside the function.
- *
- * @param dialog A pointer to a dialog struct to populate, or 0.
- * @param height The height of the dialog. Specify with macros beginning with
- * @c DIALOG_HEIGHT_.
- * @param alignment The screen position to align the dialog with. One of
- * @c DIALOG_ALIGN_TOP, @c DIALOG_ALIGN_CENTER or @c DIALOG_ALIGN_BOTTOM.
- * @param title The string to display in the title bar of the dialog.
- * @param unknown2 Unknown parameter. Value 0 should be passed.
- * @param unknown3 Unknown parameter. Value 0 should be passed.
- * @param keyboard The keyboard to display when the dialog is shown. Specify
- * with macros beginning with @c DIALOG_KBD_.
- * @return A pointer to the dialog struct created.
- */
 extern "C"
 struct GUI_Dialog *GUI_CreateDialog(
 	void *dialog,
@@ -151,35 +40,6 @@ struct GUI_Dialog *GUI_CreateDialog(
 	int keyboard
 );
 
-/**
- * Creates a label.
- *
- * If 0 is passed for the argument @p label, a label struct will be allocated
- * inside the function.
- *
- * If the flag @c LABEL_SELECTABLE is specified, when the label is in focus the
- * text and background color switch. Specifying @c LABEL_BACKGROUND enables
- * displaying the background color of the label.
- *
- * @param label A pointer to a label struct to populate, or 0.
- * @param x The X position to place the label at, in pixels and relative to the
- * top left of the display.
- * @param y The Y position to place the label at, in pixels and relative to the
- * top left of the display.
- * @param text The text to display as the label.
- * @param unknown0 Unknown parameter. Value 0 should be passed.
- * @param flags A bitfield of flags, customizing how the label should behave.
- * @param font A pointer to the font to use for the label, or 0 for the default
- * font.
- * @param textColor A pointer to an RGB565 color to use for the text, or 0 for
- * the default color.
- * @param backgroundColor A pointer to an RGB565 color to use for the
- * background, or 0 for the default color.
- * @param showShadow True if the text shadow should be shown.
- * @param shadowColor An RGB565 color to use for the text shadow.
- * @param unknown1 Unknown parameter. Value 0 should be passed.
- * @return A pointer to the label struct created.
- */
 extern "C"
 void *GUI_CreateLabel(
 	void *label,
@@ -193,30 +53,6 @@ void *GUI_CreateLabel(
 	int unknown1
 );
 
-/**
- * Creates a radio button.
- *
- * If 0 is passed for the argument @p radioButton, a radio button struct will be
- * allocated inside the function.
- *
- * The flag @c RADIOBUTTON_ENABLED must be set in the parameter @p flags for the
- * radio button to be interactive. A radio button can be set as selected by
- * default with the @c RADIOBUTTON_SELECTED flag.
- *
- * @param radioButton A pointer to a radio button struct to populate, or 0.
- * @param x The X position to place the radio button at, in pixels and relative
- * to the top left of the display.
- * @param y The Y position to place the radio button at, in pixels and relative
- * to the top left of the display.
- * @param text The text to display to the right-hand side of the radio button.
- * @param unknown0 Unknown parameter. Value 0 should be passed.
- * @param flags A bitfield of flags, customizing how the radio button should
- * behave.
- * @param font A pointer to the font to use for the radio button's label, or 0
- * for the default font.
- * @param unknown2 Unknown parameter. Value 0 should be passed.
- * @return A pointer to the radio button struct created.
- */
 extern "C"
 void *GUI_CreateRadioButton(
 	void *radioButton,
@@ -228,41 +64,6 @@ void *GUI_CreateRadioButton(
 	int unknown2
 );
 
-/**
- * Creates a text box.
- *
- * If 0 is passed for the argument @p textBox, a text box struct will be
- * allocated inside the function.
- *
- * The flag @c TEXTBOX_EDITABLE must be set in @p flags if the content of the
- * text box should be editable by the user. If it is not set, the content of the
- * text box is read-only. The flag @c TEXTBOX_DRAW_BOX must be set for the text
- * box to render correctly.
- *
- * With an example value for the parameter @p maxLength of 4, the behaviour of
- * @p countLengthByBytes is as follows:
- * - If true, 4 bytes worth of text can be written in the text box. This may be
- * two 2-byte characters, one 2-byte character and two 1-byte characters, etc.
- * - If false, 4 characters worth of text can be written in the text box. This
- * may be four 2-byte characters, one 2-byte character and three 1-byte
- * characters, etc.
- *
- * @param textBox A pointer to a text box struct to populate, or 0.
- * @param x The X position to place the text box at, in pixels and relative to
- * the top left of the display.
- * @param y The Y position to place the text box at, in pixels and relative to
- * the top left of the display.
- * @param width The width of the text box, in pixels.
- * @param text A string to pre-populate the text box with, or 0 if the text box
- * should be initially empty.
- * @param unknown0 Unknown parameter. Value 0 should be passed.
- * @param flags A bitfield of flags, customizing how the text box should behave.
- * @param maxLength The maximum number of characters/bytes the text box should
- * hold.
- * @param countLengthByBytes True if @p maxLength specifies the maximum number
- * of bytes, and false if it specifies the maximum number of characters.
- * @return A pointer to the text box struct created.
- */
 extern "C"
 void *GUI_CreateTextBox(
 	void *textBox,
@@ -316,3 +117,350 @@ void *GUI_DisplayMessageBox_Internal(
 	const char *contentPrefix, const char *contentString,
 	int buttons, bool disableCloseButton
 );
+
+class Wrapped {
+public:
+	/**
+	 * Returns a pointer to the internal class this class wraps.
+	 * 
+	 * @return The pointer to the internal class this class wraps.
+	 */
+	virtual void *GetWrapped() = 0;
+};
+
+class GUIElement : public Wrapped {};
+
+class GUIDialog : public Wrapped {
+public:
+	enum Height : int {
+		Height25 = 0,
+		Height55 = 1,
+		Height75 = 2,
+		Height95 = 3,
+		Height35 = 4,
+		Height60 = 5
+	};
+
+	enum Alignment : int {
+		AlignTop = 0,
+		AlignCenter = 1,
+		AlignBottom = 2
+	};
+
+	enum KeyboardState : int {
+		KeyboardStateNone = 0, // 2 gives same effect
+		KeyboardStateMath1 = 1, // 3 gives same effect
+		KeyboardStateMath2 = 4,
+		KeyboardStateMath3 = 5,
+		KeyboardStateTrig = 6,
+		KeyboardStateVar = 7,
+		KeyboardStateABC = 8,
+		KeyboardStateCatalog = 9,
+		KeyboardStateAdvance = 10,
+		KeyboardStateNumber = 11
+	};
+
+	/**
+	 * Creates a dialog.
+	 * 
+	 * @param height The height of the dialog.
+	 * @param alignment The screen position to align the dialog with.
+	 * @param title The string to display in the title bar of the dialog.
+	 * @param keyboard The keyboard to display when the dialog is shown.
+	 */
+	GUIDialog(
+		enum Height height, enum Alignment alignment,
+		const char* title,
+		enum KeyboardState keyboard
+	) {
+		m_wrapped = reinterpret_cast<decltype(m_wrapped)>(GUI_CreateDialog(
+			0,
+			height, alignment,
+			title,
+			0, 0,
+			keyboard
+		));
+	}
+
+	/**
+	 * Returns the X position of the left edge of the dialog body, in pixels.
+	 *
+	 * @return The X position of the left edge of the dialog body, in pixels.
+	 */
+	uint16_t GetLeftX() {
+		return m_wrapped->leftX;
+	}
+
+	/**
+	 * Returns the Y position of the top edge of the dialog body, in pixels.
+	 *
+	 * @return The Y position of the top edge of the dialog body, in pixels.
+	 */
+	uint16_t GetTopY() {
+		return m_wrapped->topY;
+	}
+
+	/**
+	 * Returns the X position of the right edge of the dialog body, in pixels.
+	 *
+	 * @return The X position of the right edge of the dialog body, in pixels.
+	 */
+	uint16_t GetRightX() {
+		return m_wrapped->rightX;
+	}
+
+	/**
+	 * Returns the Y position of the bottom edge of the dialog body, in pixels.
+	 *
+	 * @return The Y position of the bottom edge of the dialog body, in pixels.
+	 */
+	uint16_t GetBottomY() {
+		return m_wrapped->bottomY;
+	}
+
+	/**
+	 * Adds a GUI element to the dialog.
+	 * 
+	 * @param element The GUI element to add.
+	 */
+	void AddElement(GUIElement &element) {
+		m_wrapped->vtable->AddElement(m_wrapped, element.GetWrapped(), 0);
+	}
+
+	/**
+	 * Presents the dialog to the user. Blocks until the dialog is closed.
+	 */
+	void ShowDialog() {
+		m_wrapped->vtable->ShowDialog(m_wrapped);
+	}
+
+	void *GetWrapped() {
+		return m_wrapped;
+	}
+
+private:
+	struct {
+		uint8_t unknown0[0x10];
+
+		// refer to accessors for documentation
+		uint16_t leftX;
+		uint16_t topY;
+		uint16_t rightX;
+		uint16_t bottomY;
+
+		uint8_t unknown1[0x34];
+
+		struct {
+			uint8_t unknown0[0x38];
+
+			// unknown0 - always pass 0
+			void (*const AddElement)(void *dialog, void *element, int unknown0);
+
+			uint8_t unknown1[0x158];
+			void (*const ShowDialog)(void *dialog);
+
+			// TODO: work out the true length of the vtable
+			uint8_t unknown2[0x200];
+		} *vtable;
+	} *m_wrapped;
+};
+
+class GUILabel : public GUIElement {
+public:
+	enum Flag : int {
+		/** Enables displaying the background color of the label. */
+		FlagBackground = 1 << 0,
+
+		/**
+		 * Allows the label to be selected/brought into focus.
+		 * 
+		 * When the label is selected, the text and background colors switch.
+		 */
+		FlagSelectable = 1 << 15
+	};
+
+	/**
+	 * Creates a label.
+	 * 
+	 * @param x The X position to place the label at, in pixels and relative to
+	 * the top left of the display.
+	 * @param y The Y position to place the label at, in pixels and relative to
+	 * the top left of the display.
+	 * @param text The text to display as the label.
+	 */
+	GUILabel(int x, int y, const char *text) : GUILabel(
+		x, y, text, 0, 0, 0, false, 0
+	) {
+
+	}
+
+	/**
+	 * Creates a label.
+	 * 
+	 * @param x The X position to place the label at, in pixels and relative to
+	 * the top left of the display.
+	 * @param y The Y position to place the label at, in pixels and relative to
+	 * the top left of the display.
+	 * @param text The text to display as the label.
+	 * @param flags A bitfield of flags specified by bitwise-ORing members of
+	 * the @ref Flag enum.
+	 * @param textColor A pointer to an RGB565 color to use for the text, or 0
+	 * for the default color.
+	 * @param backgroundColor A pointer to an RGB565 color to use for the
+	 * background, or 0 for the default color.
+	 */
+	GUILabel(
+		int x, int y,
+		const char *text,
+		int flags,
+		uint16_t *textColor, uint16_t *backgroundColor
+	) : GUILabel(x, y, text, flags, textColor, backgroundColor, false, 0) {
+
+	}
+
+	/**
+	 * Creates a label.
+	 * 
+	 * @param x The X position to place the label at, in pixels and relative to
+	 * the top left of the display.
+	 * @param y The Y position to place the label at, in pixels and relative to
+	 * the top left of the display.
+	 * @param text The text to display as the label.
+	 * @param flags A bitfield of flags specified by bitwise-ORing members of
+	 * the @ref Flag enum.
+	 * @param textColor A pointer to an RGB565 color to use for the text, or 0
+	 * for the default color.
+	 * @param backgroundColor A pointer to an RGB565 color to use for the
+	 * background, or 0 for the default color.
+	 * @param showShadow True if the text shadow should be shown.
+	 * @param shadowColor An RGB565 color to use for the text shadow.
+	 */
+	GUILabel(
+		int x, int y,
+		const char *text,
+		int flags,
+		uint16_t *textColor, uint16_t *backgroundColor,
+		bool showShadow, uint16_t shadowColor
+	) {
+		m_wrapped = reinterpret_cast<decltype(m_wrapped)>(GUI_CreateLabel(
+			0,
+			x, y, text,
+			0,
+			flags,
+			0, 
+			textColor, backgroundColor, showShadow, shadowColor,
+			0
+		));
+	}
+
+	void *GetWrapped() {
+		return m_wrapped;
+	}
+
+private:
+	void *m_wrapped;
+};
+
+class GUIRadioButton : public GUIElement {
+public:
+	enum Flag : int {
+		/** Causes the radio button to be selected by default. */
+		FlagSelected = 1 << 2,
+
+		/** Makes the radio button interactive. */
+		FlagEnabled = 1 << 15
+	};
+
+	/**
+	 * Creates a radio button.
+	 * 
+	 * @param x The X position to place the radio button at, in pixels and
+	 * relative to the top left of the display.
+	 * @param y The Y position to place the radio button at, in pixels and
+	 * relative to the top left of the display.
+	 * @param text The text to display to the right-hand side of the radio
+	 * button.
+	 * @param flags A bitfield of flags specified by bitwise-ORing members of
+	 * the @ref Flag enum.
+	 */
+	GUIRadioButton(
+		int x, int y,
+		const char *text,
+		int flags
+	) {
+		m_wrapped = reinterpret_cast<decltype(m_wrapped)>(GUI_CreateRadioButton(
+			0,
+			x, y,
+			text,
+			0,
+			flags,
+			0, 0
+		));
+	}
+
+	void *GetWrapped() {
+		return m_wrapped;
+	}
+
+private:
+	void *m_wrapped;
+};
+
+class GUITextBox : public GUIElement {
+public:
+	enum Flag {
+		/** Enables drawing the text box's outline and background. */
+		FlagDrawBox = 1 << 3,
+
+		/** Allows the contents of the text box to be modified. */
+		FlagEditable = 1 << 8
+	};
+
+	/**
+	 * Creates a text box.
+	 *
+	 * With an example value for the parameter @p maxLength of 4, the behaviour
+	 * of @p countLengthByBytes is as follows:
+	 * - If true, 4 bytes worth of text can be written in the text box. This may
+	 * be two 2-byte characters, one 2-byte character and two 1-byte characters,
+	 * etc.
+	 * - If false, 4 characters worth of text can be written in the text box.
+	 * This may be four 2-byte characters, one 2-byte character and three 1-byte
+	 * characters, etc.
+	 * 
+	 * @param x The X position to place the text box at, in pixels and relative
+	 * to the top left of the display.
+	 * @param y The Y position to place the text box at, in pixels and relative
+	 * to the top left of the display.
+	 * @param width The width of the textbox, in pixels.
+	 * @param text A string to pre-populate the text box with, or 0 if the
+	 * textbox should be initially empty.
+	 * @param flags A bitfield of flags specified by bitwise-ORing members of
+	 * the @ref Flag enum.
+	 * @param maxLength The maximum number of characters/bytes the text box
+	 * should hold.
+	 * @param countLengthByBytes True if @p maxLength specifies the maximum
+	 * number of bytes, and false if it specifies the maximum number of
+	 * characters.
+	 */
+	GUITextBox(
+		int x, int y, int width,
+		const char *text,
+		int flags, int maxLength, bool countLengthByBytes
+	) {
+		m_wrapped = reinterpret_cast<decltype(m_wrapped)>(GUI_CreateTextBox(
+			0,
+			x, y, width,
+			text,
+			0,
+			flags, maxLength, countLengthByBytes
+		));
+	}
+
+	void *GetWrapped() {
+		return m_wrapped;
+	}
+
+private:
+	void *m_wrapped;
+};
