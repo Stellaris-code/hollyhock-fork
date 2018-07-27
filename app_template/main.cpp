4a48dec22e81ac38090215c0686a7d3b60785579
The6P4C <watsonjcampbell@gmail.com>
2018-07-27 22:19:06 +1000

    Improve the app template

--- app_template/main.cpp
+++ app_template/main.cpp
@@ -1,19 +1,15 @@
 #include <appdef.h>
-#include <sdk/os/lcd.hpp>
-#include <stdint.h>
 
-APP_NAME("App Template")
-APP_DESCRIPTION("This is the description")
-APP_AUTHOR("The6P4C")
+/*
+ * Fill this section in with some information about your app.
+ * All fields are optional - so if you don't need one, take it out.
+ */
+APP_NAME("My app name")
+APP_DESCRIPTION("A short description of my app")
+APP_AUTHOR("My name")
 APP_VERSION("1.0.0")
 
 extern "C"
 void main() {
-    uint16_t *vram = LCD_GetVRAMAddress();
-
-    for (int i = 0; i < 1000; ++i) {
-        vram[i] = 0xF00F;
-    }
-
-    LCD_Refresh();
+    // Put your app's code here!
 }
